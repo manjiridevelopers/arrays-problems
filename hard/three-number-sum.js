6b292aaf8412d0f6a86592c1bfdd87d2e07d1daf
unknown <manjiri.developers@gmail.com>
2023-06-20 17:46:01 +0530

    removing duplicates

--- hard/three-number-sum.js
+++ hard/three-number-sum.js
@@ -10,6 +10,8 @@ function threeNumSum(arr, targetSum) {
     let left = i + 1;
     let right = arr.length - 1;
 
+    if (i > 0 && arr[i] == arr[i - 1]) continue;
+
     while (left < right) {
       let sum = curNum + arr[left] + arr[right];
 
@@ -18,6 +20,10 @@ function threeNumSum(arr, targetSum) {
         result.push(ans);
         left++;
         right--;
+
+        /* avoiding duplicates */
+        while (left < right && arr[left] == arr[left - 1]) left++;
+        while (left < right && arr[right] == arr[right + 1]) right--;
       } else if (sum < targetSum) {
         left++;
       } else if (sum > targetSum) {
